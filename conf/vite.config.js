92ba36cfb9ffeee13409bfed56d8257bf0cbf6fa
Limezy <arnaudetpia@de-ruffray.fr>
2022-08-31 16:17:36 +0700

    New try for the port...

---
+++ conf/vite.config.js
@@ -0,0 +1,16 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	resolve: {
+		alias: {
+			'xmlhttprequest-ssl': './node_modules/engine.io-client/lib/xmlhttprequest.js',
+			'@api': path.resolve('./src/api')
+		}
+	},
+	plugins: [sveltekit()],
+	server: {
+    	port: __PORT__,
+  	},
+};
