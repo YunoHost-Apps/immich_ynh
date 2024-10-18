c5695d3066c710b3e0b76366c36eac4895d006b7
Sylvain <syl@thinkpad.localhost>
2024-10-18 23:13:20 +0200

    Fix

--- conf/immich-machine-learning-start.sh
+++ conf/immich-machine-learning-start.sh
@@ -8,7 +8,7 @@ cd "__INSTALL_DIR__/app/machine-learning"
 source venv/bin/activate
 
 : "${IMMICH_HOST:=127.0.0.1}"
-: "${IMMICH_PORT:=__PORT_MACHINELEARNING__}"
+: "${MACHINE_LEARNING_PORT:=__PORT_MACHINELEARNING__}"
 : "${MACHINE_LEARNING_WORKERS:=1}"
 : "${MACHINE_LEARNING_WORKER_TIMEOUT:=300}"
 : "${MACHINE_LEARNING_HTTP_KEEPALIVE_TIMEOUT_S:=2}"
@@ -16,7 +16,7 @@ source venv/bin/activate
 exec gunicorn app.main:app \
         -k app.config.CustomUvicornWorker \
         -c gunicorn_conf.py \
-        -b "$IMMICH_HOST":"$IMMICH_PORT" \
+        -b "$IMMICH_HOST":"$MACHINE_LEARNING_PORT" \
         -w "$MACHINE_LEARNING_WORKERS" \
         -t "$MACHINE_LEARNING_WORKER_TIMEOUT" \
         --log-config-json log_conf.json \
