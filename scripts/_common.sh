d8dfaa924152de486f4df141e854c2fa48b8e3bb
Sylvain <syl@thinkpad.localhost>
2025-09-06 09:35:44 +0200

    Fix trixie

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -131,18 +131,26 @@ myynh_install_immich() {
 			local uv="/usr/local/bin/uv"
 		# Execute in a subshell
 		(
+			# Define some options for uv
+				export UV_PYTHON_INSTALL_DIR="$install_dir/app/machine-learning"
+				export UV_NO_CACHE=true
+				export UV_NO_MODIFY_PATH=true
 			# Create the virtual environment
 				"$uv" venv --quiet "$install_dir/app/machine-learning/venv" --python "$(app_py_version)"
-			# activate the virtual environment
+			# Activate the virtual environment
 				set +o nounset
 				source "$install_dir/app/machine-learning/venv/bin/activate"
 				set -o nounset
-			# add pip
+			# Add pip
 				"$uv" pip --quiet --no-cache-dir install --upgrade pip
-			# add uv
+			# Add uv
 				ynh_hide_warnings "$install_dir/app/machine-learning/venv/bin/pip" install --no-cache-dir --upgrade uv
-			# uv install
+			# Install with uv
 				ynh_hide_warnings "$install_dir/app/machine-learning/venv/bin/uv" sync --quiet --no-install-project --no-install-workspace --extra cpu --no-cache --active --link-mode=copy
+			# Clear uv options
+				unset UV_PYTHON_INSTALL_DIR
+				unset UV_NO_CACHE
+				unset UV_NO_MODIFY_PATH
 		)
 		# Copy built files
 			cp -a "$source_dir/machine-learning/ann" "$install_dir/app/machine-learning/"
