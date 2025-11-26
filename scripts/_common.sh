1c2ca4e3092fcc2aea30c3bd38c46abc2336efab
ewilly <ewilly@free.fr>
2025-11-26 22:59:59 +0100

    Fix 323

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -126,6 +126,7 @@ myynh_install_immich() {
 		# Definie pnpm & mise options
 			export PNPM_HOME="$source_dir/pnpm"
 			export MISE_DATA_DIR="$source_dir/mise"
+			export MISE_CACHE_DIR="$MISE_DATA_DIR/cache"
 		# Build server
 			cd "$source_dir/server"
    			export SHARP_IGNORE_GLOBAL_LIBVIPS=true
@@ -176,6 +177,7 @@ myynh_install_immich() {
 			#ynh_hide_warnings mise implode
 			unset PNPM_HOME
 			unset MISE_DATA_DIR
+			unset MISE_CACHE_DIR
  			unset SHARP_IGNORE_GLOBAL_LIBVIPS
 
 	# Install immich-machine-learning
