e4fd88bf83f1dc0352d75e87bf7e33d13e48053b
ewilly <6552172+ewilly@users.noreply.github.com>
2026-03-03 07:50:53 +0100

    fix restore db script

--- conf/restore_immich_db_backup.sh
+++ conf/restore_immich_db_backup.sh
@@ -46,7 +46,10 @@ then
 	myynh_drop_psql_db 1>/dev/null
 
 	ynh_print_info "[##+.................] Creating an empty immich db..."
-	myynh_create_psql_db 1>/dev/null
+	if [[ $YNH_DEBIAN_VERSION == "bookworm" ]]
+	then
+		myynh_provision_postgresql 1>/dev/null
+	fi
 	myynh_update_psql_db 1>/dev/null
 
 	ynh_print_info "[###++++++++++++++++.] Restoring immich db backup... (Depending on your database size, this may take a long while)"
