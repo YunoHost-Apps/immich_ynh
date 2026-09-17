7e8469ac6727066db2f8fea3751e9dc68cdd0bd7
ewilly <6552172+ewilly@users.noreply.github.com>
2026-09-17 20:58:32 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,7 @@
 # Postgresql version
 psql_version=17
 db_cluster="$psql_version/main"
+db_cluster_renamed="17/renamed" # For Bookworm -> Trixie postgresql migration
 
 # Fail2ban
 failregex="$app-server.*Failed login attempt for user.+from ip address\s?<ADDR>"
@@ -112,7 +113,7 @@ myynh_provision_postgresql() {
 	ynh_print_info "Provisionning database on postgresql $psql_version..."
 
 	# Create the cluster if not existing
-	if ! pg_lsclusters | grep -q "$db_cluster"
+	if ! pg_lsclusters | grep -wq "$db_cluster"
 	then
 		pg_createcluster ${db_cluster/\// } --start
 	fi
@@ -168,15 +169,14 @@ myynh_set_default_back_to_debian() {
 # 4. drop the cluster 17/renamed
 # (settings and env values are updated after with myynh_update_psql_db)
 myynh_migrate_cluster_if_needed() {
-	# Definie local var
-	local db_cluster_renamed
-
 	# Check if the cluster 17/renamed exists
-	db_cluster_renamed="17/renamed"
-	if pg_lsclusters | grep -q "$db_cluster_renamed"
+	if pg_lsclusters | grep -wq "$db_cluster_renamed"
 		then
 		# Dump the immich db from the cluster 17/renamed
 		myynh_dump_psql_db --cluster="$db_cluster_renamed"
+
+		# Drop the provisionned immich db on the cluster 17/main
+		myynh_drop_psql_db
 
 		# Restore the immich db dump to the cluster 17/main
 		myynh_restore_psql_db
